35b26844ecd71241e84b5418814e02bd23ec82a3
이찬호 <0407chan@naver.com>
2020-07-21 14:02:49 +0900

    [추가] 백엔드 카드 dao 설정 (#21)
    cardRepository 추가
    cardService 추가
    cardModel 추가
    cardRouter 추가

---
+++ backEnd/repositories/cardRepository.js
@@ -0,0 +1,44 @@
+let sql = require("../config/database.js");
+let cardModel = require("../models/card");
+
+async function createCard(cardParam) {
+  let card = new cardModel.Card(cardParam);
+  return new Promise((resolve, reject) => {
+    sql.query("insert into card set ?", card, function (err, res) {
+      if (err) {
+        return reject(err.code);
+      }
+      return resolve(res.insertId);
+    });
+  }).catch(function (err) {
+    return err;
+  });
+}
+
+async function getAllCards() {
+  return new Promise((resolve, reject) => {
+    sql.query("select * from card", function (err, res) {
+      if (err) {
+        return reject(err.code);
+      }
+      return resolve(res);
+    });
+  }).catch(function (err) {
+    return err;
+  });
+}
+
+async function getCardById(id) {
+  return new Promise((resolve, reject) => {
+    db.query(`select * from card WHERE id = ${id}`, function (err, res) {
+      if (err) {
+        reject(err.code);
+      }
+      resolve(res);
+    });
+  }).catch(function (err) {
+    return err;
+  });
+}
+
+module.exports = { createCard, getAllCards, getCardById };
