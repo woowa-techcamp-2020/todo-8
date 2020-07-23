4d4cb38c0d2a46f167068988bcad17b2b9a573f4
이찬호 <0407chan@naver.com>
2020-07-23 21:25:50 +0900

    [추가] 백엔드 todo router 추가

--- backEnd/routes/index.js
+++ backEnd/routes/index.js
@@ -4,11 +4,11 @@ var router = express.Router();
 var userRouter = require("./userRouter");
 var cardRouter = require("./cardRouter");
 var columnRouter = require("./columnRouter");
-
+var todoRouter = require("./todoRouter");
 router.use("/api", userRouter);
 router.use("/api", cardRouter);
 router.use("/api", columnRouter);
-
+router.use("/api", todoRouter);
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
