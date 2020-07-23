e05f23c375f26614b263b4ec02793e4ddc264791
이찬호 <0407chan@naver.com>
2020-07-24 01:48:49 +0900

    [수정] 리스트에 추가 버튼 적용
    - 잘됩니다

--- frontEnd/src/lib/columnService.js
+++ frontEnd/src/lib/columnService.js
@@ -1,10 +1,11 @@
 import api from "../api/index";
 import store from "../store/index";
 import Column from "../components/Column";
+import listService from "../lib/listService";
 
 function columnService() {
   let columns = store.state.columns;
-  columns.forEach((col) => {
+  columns.forEach(async (col) => {
     let column = new Column(`#column-${col.column_id}`);
     column.addComponent({
       name: "cards",
@@ -12,32 +13,34 @@ function columnService() {
         cards: [],
       },
       view(model) {
-        return `
-            <li class="start"></li>
+        return `<li class="start">
+              <div class="card-count">${model.cards.length}</div>
+              <span class="list-title">${col.column_title}</span>
+            </li>
+            <div class="card-list-wrapper">
             ${model.cards
               .map(
-                (card) => `<li class="card"><div ">
+                (card) => `<li class="card ${card.id}"><div ">
                 <div>
                     ${card.contents}
                 </div>
                 <div>
-                    Addad by <span class="card-user">${card.userId}</span>
-                </div>
-                <div>
-                    ${card.created_at}
+                    <span class="card-des">Addad by</span> <span class="card-user">${card.userId}</span>
                 </div>
             </div> 
             </li>`
               )
               .join("")}
-        `;
+            </div>`;
       },
       async controller(model) {
         const cards = await api.Card().getCardByColumnId(col.column_id);
-
         model.cards = cards;
         column.updateView();
-        console.log("dont");
+
+        let listServiceInstance = new listService();
+        listServiceInstance.addlistButtonsTo(column.columnElement);
+        listServiceInstance.hideAddCardModal(column.columnElement);
       },
     });
 
